f659a2ba723d78e1f5c7be20463f14329d205676
Kevin Hou <k@exafunction.com>
2023-12-06 14:46:16 -0800

    Style change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { NewReview } from "@/components/NewReview/NewReview";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-start p-24 space-y-8">
       <h1>My Movie Reviews</h1>
       <NewReview />
       <MovieGrid />
