2864c1e83afc46b87b835e6a13826bd9c4fdcc37
Kevin Hou <k@exafunction.com>
2023-12-05 18:57:14 -0800

    Add ShadCN

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import { MovieGrid } from "@/components/MovieGrid/MovieGrid";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>My Movie Reviews</h1>
+      <Button>Add Review</Button>
       <MovieGrid />
     </main>
   );
