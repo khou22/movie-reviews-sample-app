0deab3a5d4374109f095fea719e9776817042cc3
Kevin Hou <k@exafunction.com>
2023-12-05 19:31:59 -0800

    Add dialogue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,11 @@
 import { MovieGrid } from "@/components/MovieGrid/MovieGrid";
-import { Button } from "@/components/ui/button";
-import { PlusIcon } from "@radix-ui/react-icons";
+import { NewReview } from "@/components/NewReview/NewReview";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>My Movie Reviews</h1>
-      <Button>
-        <PlusIcon className="mr-2 h-4 w-4" /> Add Review
-      </Button>
+      <NewReview />
       <MovieGrid />
     </main>
   );
---
+++ src/components/NewReview/NewReview.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Button } from "../ui/button";
+import { PlusIcon } from "@radix-ui/react-icons";
+
+export const NewReview = () => {
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button>
+          <PlusIcon className="mr-2 h-4 w-4" /> Add Review
+        </Button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Are you sure absolutely sure?</DialogTitle>
+          <DialogDescription>
+            This action cannot be undone. This will permanently delete your
+            account and remove your data from our servers.
+          </DialogDescription>
+        </DialogHeader>
+      </DialogContent>
+    </Dialog>
+  );
+};
