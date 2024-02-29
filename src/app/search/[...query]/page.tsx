85e276e72f0ed887c9855927920fc07f1969bbed
Kevin Hou <k@exafunction.com>
2024-02-29 00:33:23 -0800

    Add basic search template

--- src/app/search/[...query]/page.tsx
+++ src/app/search/[...query]/page.tsx
@@ -1,3 +1,6 @@
+import { MovieReview } from "@/models";
+import { getMongoUri, mongoDbCollectionId, mongoDbId } from "@/utils/mongodb";
+import { MongoClient } from "mongodb";
 import { NextPage } from "next";
 
 type SearchPageParams = {
@@ -10,10 +13,14 @@ const SearchPage: NextPage<SearchPageParams> = async ({
   params: { query },
 }) => {
   const queryStr = query.join("");
+  let movies: MovieReview[] = [];
+
+  const mongo = new MongoClient(getMongoUri());
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-start p-24 space-y-8">
       <h1>Search Results: &quot;{queryStr}&quot;</h1>
+      <p>Results: {movies.length}</p>
     </main>
   );
 };
